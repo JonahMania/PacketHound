60211ce7c6e815f68f352699299c538eb97b8a00
jonahmania <jmania999@gmail.com>
2016-11-21 22:36:18 -0500

    Add command to install pacCap modules

--- scripts/install.js
+++ scripts/install.js
@@ -5,35 +5,45 @@ const config = require("../webpack.config.js");
 
 //Set webpack compiler config to root config for webpack
 var compiler = webpack(config);
-//Run node-gyp configure for pacCap
-console.log("Configuring pacCap...");
-exec('node-gyp configure',{cwd:"src/pacCap"}, function(error, stdout, stderr){
-    if (error) {
+//Installing packages for pacCap
+exec( 'npm install',{cwd:"src/pacCap"}, function(error, stdout, stderr ){
+     if (error) {
         console.error(error);
     }else{
         console.log(stdout);
         console.log(stderr);
-        console.log("Configure finished");
-        console.log("Building pacCap...");
-        //Run node-gyp build for pacCap
-        exec('node-gyp build',{cwd:"src/pacCap"}, function(error, stdout, stderr){
+        //Run node-gyp configure for pacCap
+        console.log("Configuring pacCap...");
+        exec('node-gyp configure',{cwd:"src/pacCap"}, function(error, stdout, stderr){
             if (error) {
                 console.error(error);
             }else{
                 console.log(stdout);
                 console.log(stderr);
-                console.log("Build finished");
-                // Run webpack compiler
-                console.log("Building app...");
-                compiler.run(function(error, stats) {
-                    if(error){
+                console.log("Configure finished");
+                console.log("Building pacCap...");
+                //Run node-gyp build for pacCap
+                exec('node-gyp build',{cwd:"src/pacCap"}, function(error, stdout, stderr){
+                    if (error) {
                         console.error(error);
                     }else{
+                        console.log(stdout);
+                        console.log(stderr);
                         console.log("Build finished");
+                        // Run webpack compiler
+                        console.log("Building app...");
+                        compiler.run(function(error, stats) {
+                            if(error){
+                                console.error(error);
+                            }else{
+                                console.log("Build finished");
+                            }
+                        });
                     }
                 });
             }
         });
     }
-});
+})
+
 
